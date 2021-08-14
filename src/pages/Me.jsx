3bd2ef9a91ecf1c7002f3851dfa7d62bc9ffe9c8
GentritBegaj <gentritbegaj@gmail.com>
2021-08-14 13:04:34 +0200

    ok

--- src/pages/Me.jsx
+++ src/pages/Me.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useStateValue } from '../contextAPI/StateProvider';
 import { TextField, Button, Paper, Avatar } from '@material-ui/core';
 import { NavBar } from '../components';
