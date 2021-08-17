84dac30b6be8d9131877c060bfed9af4e801c869
GentritBegaj <gentritbegaj@gmail.com>
2021-08-17 18:00:28 +0200

    https

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Messages from './pages/Messages';
 import { io } from 'socket.io-client';
 import { Banner } from './components/Banner';
 
-export const socket = io(`http://api.rideshareapp.xyz`, {
+export const socket = io(`https://api.rideshareapp.xyz`, {
   withCredentials: true,
   transports: ['websocket'],
 });
