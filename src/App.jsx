881cf53f0b0841ae1532b93edd4eab5574e4580a
GentritBegaj <gentritbegaj@gmail.com>
2021-08-17 18:47:31 +0200

    env

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Messages from './pages/Messages';
 import { io } from 'socket.io-client';
 import { Banner } from './components/Banner';
 
-export const socket = io(`https://api.rideshareapp.xyz`, {
+export const socket = io(process.env.REACT_APP_BACKEND_URL, {
   withCredentials: true,
   transports: ['websocket'],
 });
